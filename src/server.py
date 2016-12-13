4059ffa2fc9adb5e238973ebd7af6253c0ad0528
Marc Fieser <midfies@gmail.com>
2016-12-12 17:19:34 -0800

    added basic server funxtion

--- src/server.py
+++ src/server.py
@@ -1,1 +1,28 @@
 """Server.py for python echo server."""
+
+import socket
+
+
+def server():
+    """Place server into listening mode wating for connection."""
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 502)
+    server.bind(address)
+    server.listen(1)
+    print("Waiting for connection...")
+    conn, addr = server.accept()
+
+    # if conn:
+    #     buffer_length = 8
+    #     message_complete = False
+    #     while not message_complete:
+    #         part = conn.recv(buffer_length)
+    #         print(part.decode('utf8'))
+    #         if len(part) < buffer_length:
+    #             break
+
+    # message = "I hear you, loud and clear!"
+    # conn.sendall(message.encode('utf8'))
+
+
+server()
